68fdc2e632667671d3e40128ca4bf978630c6a44
Fabian <contacto.fabianmartinezcaro@gmail.com>
2023-05-21 19:47:40 -0300

    UI class added

---
+++ js/classes/UI.js
@@ -0,0 +1,22 @@
+export default class UI{
+
+    mostrarAlerta(contenedor, mensaje, tipo){
+        const alerta = document.createElement('P');
+        alerta.textContent = mensaje;
+        let condicionCumplida = false;
+
+        if(tipo === 'error'){
+            alerta.classList.add('error');
+            contenedor.appendChild(alerta);
+            condicionCumplida = true;
+        }
+
+        if(condicionCumplida){
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+        }
+
+    }   
+
+}
--- js/funciones.js
+++ js/funciones.js
@@ -1,10 +1,14 @@
+import UI from "./classes/UI.js";
 import { moneda, criptomoneda } from "./selectores.js";
+
+const ui = new UI;
 
 
 export function validarFormulario(evento){
     evento.preventDefault();
     
     if(moneda.value === '' || criptomoneda.value === ''){
-        console.log('error')
+        ui.mostrarAlerta();
     }
+
 }
